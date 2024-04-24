78c0534284c84b985d000e00e9289237600b2940
이름 <>
2024-04-24 23:50:20 +0900

    chore : Add lang-box

--- LineFriends/script.js
+++ LineFriends/script.js
@@ -9,3 +9,23 @@ document.querySelector('.currency_unit').addEventListener('click', function(){
         document.querySelector('.box').style.opacity = '0';    
     }
 })
+
+document.querySelector('.language').addEventListener('click', function(){
+    let lang = document.querySelector('.lang-box').style.visibility;
+
+    if(lang == '') {
+        document.querySelector('.lang-box').style.visibility = 'visible';
+        document.querySelector('.lang-box').style.opacity = '1';
+    } else if(lang == 'visible') {
+        document.querySelector('.lang-box').style.visibility = '';
+        document.querySelector('.lang-box').style.opacity = '0';
+    }
+})
+
+document.querySelector('.currency_unit').addEventListener('click', function(){
+    document.querySelector('.lang-box').style.visibility = '';
+})
+
+document.querySelector('.language').addEventListener('click', function(){
+    document.querySelector('.box').style.visibility = '';
+})
