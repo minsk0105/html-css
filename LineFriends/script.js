5f2fc5ac6c205263463e73dfaf8906a552d355fe
minsk0105 <cminseog7065@gmail.com>
2024-04-26 16:08:56 +0900

    chore : Add middle contents

--- LineFriends/script.js
+++ LineFriends/script.js
@@ -1,31 +1,47 @@
 document.querySelector('.currency_unit').addEventListener('click', function(){
-    let money = document.querySelector('.box').style.visibility;
+    const nation = document.querySelector('.box').style.visibility;
 
-    if(money == '') {
+    if(nation == '') {
         document.querySelector('.box').style.visibility = 'visible';
         document.querySelector('.box').style.opacity = '1';
-    } else if(money == 'visible') {
+        document.querySelector('.box').style.transform = 'scaleY(1)';
+    }
+    
+    else if(nation == 'visible') {
         document.querySelector('.box').style.visibility = '';
-        document.querySelector('.box').style.opacity = '0';    
+        document.querySelector('.box').style.opacity = '0';
+        document.querySelector('.box').style.transform = 'scaleY(0)';
+    }
+
+    const double = document.querySelector('.lang-box').style.visibility;
+
+    if(double == 'visible') {
+        document.querySelector('.lang-box').style.visibility = '';
+        document.querySelector('.lang-box').style.opacity = '0';
+        document.querySelector('.lang-box').style.transform = 'scaleY(0)';
     }
 })
 
 document.querySelector('.language').addEventListener('click', function(){
-    let lang = document.querySelector('.lang-box').style.visibility;
+    const lang = document.querySelector('.lang-box').style.visibility;
 
     if(lang == '') {
         document.querySelector('.lang-box').style.visibility = 'visible';
         document.querySelector('.lang-box').style.opacity = '1';
-    } else if(lang == 'visible') {
+        document.querySelector('.lang-box').style.transform = 'scaleY(1)';
+    }
+
+    else if(lang == 'visible') {
         document.querySelector('.lang-box').style.visibility = '';
         document.querySelector('.lang-box').style.opacity = '0';
+        document.querySelector('.lang-box').style.transform = 'scaleY(0)';
     }
-})
 
-document.querySelector('.currency_unit').addEventListener('click', function(){
-    document.querySelector('.lang-box').style.visibility = '';
-})
+    const cross = document.querySelector('.box').style.visibility;
 
-document.querySelector('.language').addEventListener('click', function(){
-    document.querySelector('.box').style.visibility = '';
+    if(cross == 'visible') {
+        document.querySelector('.box').style.visibility = '';
+        document.querySelector('.box').style.opacity = '0';
+        document.querySelector('.box').style.transform = 'scaleY(0)';
+    }
 })
