75541a6cc7ea1bddc3a233cbc5a79ffc102a0b1d
이름 <>
2024-05-18 00:15:09 +0900

    chore : Last edit

--- Color Flipper/script.js
+++ Color Flipper/script.js
@@ -3,8 +3,13 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
-    const randomNumber = 2; // 0 - 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
     document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = color[randomNumber];
+    color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+};
