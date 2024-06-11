f7f043c268ae2b43d9e546eba146c34fce1ad654
이름 <>
2024-06-11 22:32:48 +0900

    chore : Add index.html

--- Slide/ScaleSlide/scalesld.js
+++ Slide/ScaleSlide/scalesld.js
@@ -1,34 +1,31 @@
-const before = document.querySelector('.before');
+const prev = document.querySelector('.before');
 const next = document.querySelector('.next');
 const slider = document.querySelector('.slider');
-const box = document.querySelector('.box');
+const box = document.querySelectorAll('.box');
+const button = document.querySelector('.buttonbox');
 
-next.addEventListener('click', function() {
+const liWidth = box[0].clientWidth;
+const sliderWidth = liWidth * box.length;
+slider.style.width = `${sliderWidth}px`;
 
-    
-    if (slider.style.marginLeft == '') {
-        slider.style.marginLeft = '-300px';
-        slider.style.transition = '1s';
+let sldnum = 0;
+let translate = 0;
+
+button.addEventListener('click', moveSlide);
+
+function moveSlide(event) {
+    event.preventDefault();
+    if (event.target.className === 'next') {
+        if (sldnum !== box.length -3) {
+            translate -= liWidth;
+            slider.style.transform = `translateX(${translate}px)`;
+            sldnum += 1;
+        }
+    } else if (event.target.className === 'before') {
+        if (sldnum !== 0) {
+            translate += liWidth;
+            slider.style.transform = `translateX(${translate}px)`;
+            sldnum -= 1;
+        }
     }
-
-    if (slider.style.marginLeft == '-300px') {
-        slider.style.marginLeft = '';
-        slider.style.transition = '1s';
-        slider.appendChild(slider.firstElementChild);
-    }
-    
-});
-
-before.addEventListener('click', function() {
-
-    if (slider.style.marginLeft == '') {
-        slider.style.marginLeft = '300px'
-        slider.style.transition = '1s';
-    }
-
-    if (slider.style.marginLeft == '300px') {
-        slider.style.marginLeft = '';
-        slider.prepend(slider.lastElementChild);
-    }
-    
-});
+}
