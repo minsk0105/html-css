5a400ccd2009e3f5bc04fd6324018c30c6339390
minsk0105 <cminseog7065@gmail.com>
2024-12-05 20:46:11 +0900

    .

--- Nav/header.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu > li').addEventListener("mouseover", function(){
-    document.querySelector('.submenu').style.height = '200px';
-})
-
-document.querySelector('.menu > li').addEventListener("mouseout", function(){
-    document.querySelector('.submenu').style.height = '0px';
-})
